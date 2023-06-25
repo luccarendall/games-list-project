8ef35c8855df7e705d9484fee7fb74cabf4e28cc
LuccaRendall <luccarendall2@gmail.com>
2023-06-25 11:28:32 -0300

    Style: Formação geral no CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import './App.css';
 
 const App = () => {
   const [apiData, setApiData] = useState();
@@ -15,11 +16,11 @@ const App = () => {
         };
 
         const timeout = setTimeout(() => { // timeout de 5 segundos para mostrar mensagem c/ alerta de erro
-          const msg = "O servidor demorou para responder, tente mais tarde";
+          const msg = "O servidor demorou para responder, tente mais tarde...";
           console.error(msg);
           setErrMsg(msg);
           if (apiDataSuccessfully === true);
-          window.location.reload(); // Atualiza a página em caso de erro
+          // window.location.reload(); // Atualiza a página em caso de erro
         }, 5000);
 
         const response = await axios.get(url, { headers });
@@ -43,22 +44,22 @@ const App = () => {
 
   // lá no estado inicial setamos a mensagem de erro como strig vazia e ela só é preenchida caso dê algum problema e se isso acontecer essa mensagem é exibida em forma de span
   if (errMsg) {
-    return <span>{errMsg}</span>
+    return <span className='err1'>{errMsg}</span>
   }
 
   return (
   <div>
-      <ul className="card-game">
+      <ul className="list-game">
         {apiData !== undefined && apiData.length > 0 ? (
           apiData.map((game) => (
             <li className="game-item" key={game.id}>
-              <h2>{game.title}</h2>
+              <h2 className='title'>{game.title}</h2>
               <img src={game.thumbnail} alt={game.title} />
               <p>{game.short_description}</p>
             </li>
           ))
         ) : (
-          <p>Carregando...</p>
+          <p className='loading'></p>
         )}
       </ul>
     </div>
