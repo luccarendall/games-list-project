8612d5c1445f82826ebe1a8eb2ea198f1b4da35d
LuccaRendall <luccarendall2@gmail.com>
2023-06-25 09:40:47 -0300

    Feat: Concluindo dados do card, alterando arquivo de fetch para app. Em virtude do pouco tempo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
-import FetchComponent from './components/fetchApi';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [apiData, setApiData] = useState();
+  const [errMsg, setErrMsg] = useState('');
+
+  useEffect(() => {
+    let apiDataSuccessfully =  true; // Vou usar para verificar se a API retornou os dados com sucesso
+    const fetchAPI = async () => {
+      try {
+        const url = 'https://games-test-api-81e9fb0d564a.herokuapp.com/api/data'
+        const headers = {
+          'dev-email-address': 'luccarendall2@gmail.com'
+        };
+
+        const timeout = setTimeout(() => { // timeout de 5 segundos para mostrar mensagem c/ alerta de erro
+          const msg = "O servidor demorou para responder, tente mais tarde";
+          console.error(msg);
+          setErrMsg(msg);
+          if (apiDataSuccessfully === true);
+          window.location.reload(); // Atualiza a página em caso de erro
+        }, 5000);
+
+        const response = await axios.get(url, { headers });
+        clearTimeout(timeout); // Cancela o timeout
+        const data = response.data;
+        if (apiDataSuccessfully === true) {
+          setApiData(data);
+        }
+      } catch (error) {
+        console.error(error);
+        if (apiDataSuccessfully === true);
+      }
+    };
+
+    fetchAPI();
+    //  Garante que a requisição seja cancelada caso o componente seja desmontado ou reexecutado
+    return () => {
+      apiDataSuccessfully = false;
+    };
+  }, []);
+
+  // lá no estado inicial setamos a mensagem de erro como strig vazia e ela só é preenchida caso dê algum problema e se isso acontecer essa mensagem é exibida em forma de span
+  if (errMsg) {
+    return <span>{errMsg}</span>
+  }
+
   return (
-    <div className="App">
-      <FetchComponent />
+  <div>
+      <ul className="card-game">
+        {apiData !== undefined && apiData.length > 0 ? (
+          apiData.map((game) => (
+            <li className="game-item" key={game.id}>
+              <h2>{game.title}</h2>
+              <img src={game.thumbnail} alt={game.title} />
+              <p>{game.short_description}</p>
+            </li>
+          ))
+        ) : (
+          <p>Carregando...</p>
+        )}
+      </ul>
     </div>
   );
 }
